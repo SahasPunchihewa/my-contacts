030f89fa20a71a491cdbe26b9f7ca06ae1308d4c
Sahas <sahasmcg2995@gmail.com>
2024-11-11 08:42:24 +0530

    first commit

---
+++ my-contacts/AddView.swift
@@ -0,0 +1,18 @@
+//
+//  AddView.swift
+//  my-contacts
+//
+//  Created by Sahas Punchihewa on 2024-11-08.
+//
+
+import SwiftUI
+
+struct AddView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddView()
+}
---
+++ my-contacts/DataController.swift
@@ -0,0 +1,8 @@
+//
+//  DataController.swift
+//  my-contacts
+//
+//  Created by Sahas Punchihewa on 2024-11-08.
+//
+
+import Foundation
